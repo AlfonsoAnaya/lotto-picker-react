0add797a27649d8d2acb4abbe9f68497979e6248
AlfonsoAnaya <68916711+AlfonsoAnaya@users.noreply.github.com>
2023-02-11 10:31:35 -0300

    set up of app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+
+let numberOfRows = 10;
+let gameLength = 36;
+let picks = 7;
+
+function generateSheet() {
+    let sheet = [];
+    for (let i=0; i<gameLength; i++) {
+        sheet.push(i+1);
+    }
+    return sheet;
+}
+
+function pickNumbers(picks) {
+  let sheet = generateSheet();
+  let playNumbers = []
+  for (let i=0; i<picks; i++) {
+    let randomIndex = Math.floor(((Math.random())*(36-i)));
+    playNumbers.push(sheet[randomIndex]);
+    sheet.splice(randomIndex, 1);
+  }
+  console.log(playNumbers)
+  return playNumbers
+}
+
+pickNumbers();
+
+function PickNumbers() {
+  console.log(pickNumbers())
+  return (
+    <p>{pickNumbers()}</p>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Your numbers are:</h1>
+      <PickNumbers />
     </div>
   );
 }
