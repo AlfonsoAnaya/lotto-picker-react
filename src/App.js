a2c00bacc1813693e4a27dad09a03f727261fb92
AlfonsoAnaya <68916711+AlfonsoAnaya@users.noreply.github.com>
2023-02-22 13:37:16 -0300

    add form without proper management

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
+import Form from "./components/Form.js"
 import PickNumbers from './components/PickNumbers.js';
 import Footer from "./components/Footer"
+
 
 let numberOfRows = 10;
 
 function App() {
   return (
     <div className="App">
+      <Form />
       <h1>Your numbers are:</h1>
       {Array(numberOfRows).fill(<PickNumbers />)}
       <Footer />
---
+++ src/components/Form.js
@@ -0,0 +1,20 @@
+function Form() {
+    return (
+        <>
+            <label>
+                Select Extension of Lottery<input name="sheet" />
+            </label>
+            <label>
+                Select Number of Ricks<input name="picks" />
+            </label>
+            <label>
+                Select Number of Rows<input name="" />
+            </label>
+            <button>
+                Get Rows
+            </button>
+        </>
+    )
+}
+
+export default Form
