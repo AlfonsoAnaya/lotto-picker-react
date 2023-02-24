76487d719bfab61b6c233748703b39f22fcfd1ec
AlfonsoAnaya <68916711+AlfonsoAnaya@users.noreply.github.com>
2023-02-24 14:14:28 -0300

    first wire of form with state

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,26 @@
+import {useState} from "React"
+
 function Form() {
+    const [input, setInput] = useState({
+        "extension": 0,
+        "picks": 0,
+        "rows": 0,
+    })
+
+    function onChange() {
+        setInput(input.this.name = "")
+    }
+
     return (
         <>
             <label>
-                Select Extension of Lottery<input name="sheet" />
+                Select Extension of Lottery<input name="extension"/>
             </label>
             <label>
-                Select Number of Ricks<input name="picks" />
+                Select Number of Picks<input name="picks" />
             </label>
             <label>
-                Select Number of Rows<input name="" />
+                Select Number of Rows<input name="rows" />
             </label>
             <button>
                 Get Rows
