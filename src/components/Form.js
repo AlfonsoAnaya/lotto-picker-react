fb4107d6fbe9c847b87cedd035f63013ac130ecb
AlfonsoAnaya <68916711+AlfonsoAnaya@users.noreply.github.com>
2023-02-27 12:53:38 -0300

    prevent submit on form default

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,9 @@
 import {useState} from "react"
 
 function Form() {
-    const [extension, setExtension] = useState(0);
-    const [picks, setPicks] = useState(0);
-    const [rows, setRows] = useState(0);
+    const [extension, setExtension] = useState(39);
+    const [picks, setPicks] = useState(7);
+    const [rows, setRows] = useState(10);
 
     function handleExtensionChange(e) {
         setExtension(e.target.value)
@@ -15,22 +15,26 @@ function Form() {
         setRows(e.target.value)
     }
 
+    function handleSubmit(e) {
+        e.preventDefault();
+    }
+
     return (
-        <>
+        <form className="user-input-form" onSubmit={handleSubmit}>
             <label>
-                Select Extension of Lottery<input name="extension" onChange={handleExtensionChange}/>
+                Select Extension of Lottery<input placeholder='39' name="extension" onChange={handleExtensionChange}/>
             </label>
             <label>
-                Select Number of Picks<input name="picks" onChange={handlePicksChange}/>
+                Select Number of Picks<input placeholder="7" name="picks" onChange={handlePicksChange}/>
             </label>
             <label>
-                Select Number of Rows<input name="rows" onChange={handleRowsChange}/>
+                Select Number of Rows<input placeholder="10" name="rows" onChange={handleRowsChange}/>
             </label>
-            <button>
+            <button type="submit">
                 Get Rows
             </button>
             <p>extension {extension}, picks {picks}, rows {rows}</p>
-        </>
+        </form>
     )
 }
 
